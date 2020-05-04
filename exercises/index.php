485a5ae17d19c813288a07fdb188271fdd027764
Clemens <clemens.jurkowitsch@gmx.at>
2020-05-04 21:09:14 +0200

    Added capability to fetch all exercises from the database

--- exercises/index.php
+++ exercises/index.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
 
 if(isset($_GET["exid"])) {
 
-	if ($_GET["exid"]===0) {
+	if ($_GET["exid"] == 0) {
 	$sql = "select * from Exercise";
 	$result = $conn->query($sql);
 	while ($row = $result->fetch_assoc()) {
@@ -45,9 +45,7 @@ if(isset($_GET["exid"])) {
                 
             echo json_encode($json_exercise);
 	    }
-	} else {
-	    echo "0 results";
-	}
+	} 
 	$conn->close();
 }
 /*
